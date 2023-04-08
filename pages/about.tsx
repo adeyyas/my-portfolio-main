e565070d98dfe70a7f091fe43f9455f4f9a13a9f
Aziz Devrim YAYLA <azizdevrimy@gmail.com>
2023-04-08 15:24:08 +0300

    Updated technologies

--- pages/about.tsx
+++ pages/about.tsx
@@ -69,6 +69,8 @@ const AboutPage = () => {
           <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="Visual Studio Code" />
           <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Visual_Studio_Logo_%282013-2017%29.svg"alt="Visual Studio"/>
           <img src="https://upload.wikimedia.org/wikipedia/commons/9/9c/IntelliJ_IDEA_Icon.svg" alt="Intellij-Rider" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/JetBrains_Rider_Icon.svg" alt="Intellij-IDEA" />
+
           <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/terminal/terminal.png" alt="Terminal" />
         </div>
       </Section>
